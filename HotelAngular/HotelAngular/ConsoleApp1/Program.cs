c858d1eb5c6e7fb10ec29f922fa48b3dd4f0e9d0
YanaPrilipko <plilipkoyana@gmail.com>
2024-01-12 00:16:57 +0200

    hjd

---
+++ HotelAngular/HotelAngular/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+string Hash(string password)
+{
+    using (var sha256 = SHA256.Create())
+    {
+        var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+        var hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+        return hash;
+    }
+}
+
+var pass = Hash("test");
+var t = 1;
